c53c0a28165dab35484a55fa4b77ba3d21bfdac1
Asteron2007 <52139280+Asteron2007@users.noreply.github.com>
2024-08-22 21:14:39 +0300

    Добавьте файлы проекта.

---
+++ NpTcpSocketClientSendReceiveApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+using Socket client = new(AddressFamily.InterNetwork, 
+                            SocketType.Stream, 
+                            ProtocolType.Tcp);
+IPAddress address = IPAddress.Loopback;
+int port = 5000;
+
+Console.Write("Input data string: ");
+//string name = Console.ReadLine();
+string? dataString = Console.ReadLine();
+
+try
+{
+    // connect to server
+    await client.ConnectAsync(address, port);
+
+    // unstrategy send data to server
+    //byte[] nameBuffer = Encoding.UTF8.GetBytes(name);
+    //await client.SendAsync(nameBuffer);
+    //Console.WriteLine($"We send name {name} to server");
+
+
+    // send data with size of data
+    //byte[] dataBuffer = Encoding.UTF8.GetBytes(dataString);
+    ////await client.SendAsync(BitConverter.GetBytes(dataBuffer.Length));
+    //await client.SendAsync(dataBuffer);
+
+    //Console.WriteLine($"We send to server {dataBuffer.Length} bytes : {dataString}");
+
+    // receive data from server
+    //byte[] buffer = new byte[1024];
+    //int bites = await client.ReceiveAsync(buffer);
+    //Console.WriteLine($"Server answer in {Encoding.UTF8.GetString(buffer)}");
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"{ex.Message}");
+}
---
+++ NpTcpSocketListenerSendReceiveApp/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+IPAddress address = IPAddress.Loopback;
+int port = 5000;
+
+IPEndPoint endPoint = new(address, port);
+
+using Socket listener = new(AddressFamily.InterNetwork,
+                            SocketType.Stream,
+                            ProtocolType.Tcp);
+
+char marker = '$';
+
+try
+{
+    listener.Bind(endPoint);
+    listener.Listen();
+    Console.WriteLine($"Server {listener.LocalEndPoint} wait...");
+
+    int count = 3;
+    while(count > 0)
+    {
+        // accept client
+        using var client = await listener.AcceptAsync();
+
+        // unstrategy receive data from client
+        //byte[] buffer = new byte[1024];
+        //int bytes = await client.ReceiveAsync(buffer);
+        //Console.WriteLine($"We accept clent {Encoding.UTF8.GetString(buffer)} : {client.RemoteEndPoint}");
+
+
+        // receive size data and data
+        //byte[] sizeBuffer = new byte[4];
+        //await client.ReceiveAsync(sizeBuffer);
+        //int size = BitConverter.ToInt32(sizeBuffer, 0);
+
+        //byte[] buffer = new byte[size];
+        //int bytes = await client.ReceiveAsync(buffer);
+
+        //string message = Encoding.UTF8.GetString(buffer);
+        //Console.WriteLine($"Client {client.RemoteEndPoint} send to us {size} bytes: {message}");
+
+
+        // recieive data with marker
+        //List<byte> buffer = new();
+        //byte[] byteData = new byte[1];
+
+        //while(true)
+        //{
+        //    var byteCount = await client.ReceiveAsync(byteData);
+        //    if (byteCount == 0 || byteData[0] == marker) break;
+
+        //    buffer.Add(byteData[0]);
+        //}
+
+        //string message = Encoding.UTF8.GetString(buffer.ToArray());
+        //Console.WriteLine($"Client {client.RemoteEndPoint} send to us {buffer.ToArray().Length} bytes: {message}");
+
+
+        // send data to client
+        //byte[] timeBuffer = Encoding.UTF8.GetBytes(DateTime.Now.ToLongTimeString());
+        //await client.SendAsync(timeBuffer);
+        //Console.WriteLine($"Server send time to client {client.RemoteEndPoint}");
+
+        count--;
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+listener.Close();
